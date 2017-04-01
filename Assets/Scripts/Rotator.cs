0dfacd224aee33f638113de6fc5a8b2e0baea986
Nisal Dilshan <bcnisal@gmail.com>
2017-04-01 19:14:07 +0530

    Main menu animations
    rotator script
    some new materials
    changed camera position

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 20, 0) * Time.deltaTime);
+    }
+}
